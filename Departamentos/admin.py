9fc67c061d65168dd2899aaabe50bc35acdf4e49
quintana250 <qfrancisco086@gmail.com>
2026-04-25 22:33:36 -0300

    seguir con los contratos

--- Departamentos/admin.py
+++ Departamentos/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 
-from .models import Departamento, Inquilino, Contrato
+from .models import Departamento, Inquilino, Contrato, fotosDepartamento
 
 admin.site.register(Departamento)
 admin.site.register(Inquilino)
 admin.site.register(Contrato)
+admin.site.register(fotosDepartamento)
 
